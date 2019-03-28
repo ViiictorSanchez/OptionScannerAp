bc51a1a7f5e330f5621a54a92227c2cfa421cb13
ViiictorSanchez <ingsanchez423@gmail.com>
2019-03-28 16:51:25 -0400

    included slider in stockprofile

--- resources/views/stockprofile.blade.php
+++ resources/views/stockprofile.blade.php
@@ -622,6 +622,31 @@
     <!-- switch color Theme -->
     <script src="{{ URL::asset('assets/js/switch.js') }}"></script>
     <script src="{{ URL::asset('assets/js/darkTheme.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/slick.js') }}"></script>
+         <script>
+            $(document).ready(function(){
+                $('.charts-slide').slick({
+                    slidesToShow: 6,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 1500,
+                    arrows: false,
+                    dots: false,
+                    pauseOnHover: false,
+                    responsive: [{
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 4
+                        }
+                    }, {
+                        breakpoint: 520,
+                        settings: {
+                            slidesToShow: 3
+                        }
+                    }]
+                });
+            });
+         </script>
    
 @endsection
 
