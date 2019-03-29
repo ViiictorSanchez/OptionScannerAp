5dfce38d3b3b32dfc84218e402b43e62153995ce
victor sanchez <ingsanchez423@gmail.com>
2019-03-29 11:46:12 -0400

    textos de prueba index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -56,10 +56,10 @@
                                 <div class="table-responsive col-xl-12" >
                                     <ul class="nav nav-tabs nav-tabs-custom nav-justified" role="tablist">
                                         <li class="nav-item float-left">
-                                            <a class="nav-link active" data-toggle="tab" href="#home2" role="tab">Positions</a>
+                                            <a class="nav-link active" data-toggle="tab" href="#positions" role="tab">Positions</a>
                                         </li>
                                         <li class="nav-item">
-                                            <a class="nav-link" data-toggle="tab" href="#profile2" role="tab">Orders</a>
+                                            <a class="nav-link" data-toggle="tab" href="#orders" role="tab">Orders</a>
                                         </li>
                                     </ul>
                                 </div>
@@ -67,7 +67,7 @@
                                 <!-- tab content-->
                                 <div class="tab-content">
 
-                                    <div class="tab-pane active p-3" id="home2" role="tabpanel">
+                                    <div class="tab-pane active p-3" id="positions" role="tabpanel">
 
                                         <div class="table-responsive">
                                             <table class="table table-vertical">
@@ -174,44 +174,11 @@
                                         </div>
 
                                     </div>
-                                    <div class="tab-pane p-3" id="profile2" role="tabpanel">
+
+                                    <div class="tab-pane p-3" id="orders" role="tabpanel">
                                         <p class="font-14 mb-0">
-                                            Food truck fixie locavore, accusamus mcsweeney's marfa nulla
-                                            single-origin coffee squid. Exercitation +1 labore velit, blog
-                                            sartorial PBR leggings next level wes anderson artisan four loko
-                                            farm-to-table craft beer twee. Qui photo booth letterpress,
-                                            commodo enim craft beer mlkshk aliquip jean shorts ullamco ad
-                                            vinyl cillum PBR. Homo nostrud organic, assumenda labore
-                                            aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr,
-                                            vero magna velit sapiente labore stumptown. Vegan fanny pack
-                                            odio cillum wes anderson 8-bit.
-                                        </p>
-                                    </div>
-                                    <div class="tab-pane p-3" id="messages2" role="tabpanel">
-                                        <p class="font-14 mb-0">
-                                            Etsy mixtape wayfarers, ethical wes anderson tofu before they
-                                            sold out mcsweeney's organic lomo retro fanny pack lo-fi
-                                            farm-to-table readymade. Messenger bag gentrify pitchfork
-                                            tattooed craft beer, iphone skateboard locavore carles etsy
-                                            salvia banksy hoodie helvetica. DIY synth PBR banksy irony.
-                                            Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh
-                                            mi whatever gluten-free, carles pitchfork biodiesel fixie etsy
-                                            retro mlkshk vice blog. Scenester cred you probably haven't
-                                            heard of them, vinyl craft beer blog stumptown. Pitchfork
-                                            sustainable tofu synth chambray yr.
-                                        </p>
-                                    </div>
-                                    <div class="tab-pane p-3" id="settings2" role="tabpanel">
-                                        <p class="font-14 mb-0">
-                                            Trust fund seitan letterpress, keytar raw denim keffiyeh etsy
-                                            art party before they sold out master cleanse gluten-free squid
-                                            scenester freegan cosby sweater. Fanny pack portland seitan DIY,
-                                            art party locavore wolf cliche high life echo park Austin. Cred
-                                            vinyl keffiyeh DIY salvia PBR, banh mi before they sold out
-                                            farm-to-table VHS viral locavore cosby sweater. Lomo wolf viral,
-                                            mustache readymade thundercats keffiyeh craft beer marfa
-                                            ethical. Wolf salvia freegan, sartorial keffiyeh echo park
-                                            vegan.
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
                                         </p>
                                     </div>
                                 </div>
@@ -431,10 +398,6 @@
                                                 1.25%
                                             </td>
                                         </tr>
-
-
-
-
 
                                     </table>
                                 </div>
