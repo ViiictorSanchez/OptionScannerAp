a5a5d113e777107244455d5eff96a3d38d7277bd
Victor Sanchez <ingsanchez423@gmail.com>
2019-04-07 13:27:25 -0430

    actualizando todo

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,24 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('index');
+});
+
+Route::post('/logintradier', function(Request $request){
+
+    $user = $request->user;
+    $password = $request->password;
+
+    if($user === 'test' && $password === 'test123'){
+        return view('index');
+    }else{
+        header('Location:' . 'http://192.34.58.80/optionscanner/my-form/');
+        exit();
+    }
+
 });
 
 Route::get('{any}', 'LexaController@index');
